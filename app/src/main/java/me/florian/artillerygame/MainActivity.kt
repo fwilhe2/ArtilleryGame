c36cb35a4521c34ad5ef47c0cfb6e3239fe1c59f
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2021-03-26 12:40:03 +0100

    Depend on game lib

--- app/src/main/java/me/florian/artillerygame/MainActivity.kt
+++ app/src/main/java/me/florian/artillerygame/MainActivity.kt
@@ -2,10 +2,22 @@ package me.florian.artillerygame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import me.florian.artillerygamelib.GameWorld
+import me.florian.artillerygamelib.performPlayerMove
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val g = GameWorld()
+        do {
+            performPlayerMove(g)
+            println(g)
+        }while (g.gameContinues())
+        println(g.determineWinner())
+
+
     }
 }
